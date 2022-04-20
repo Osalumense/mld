243b10c81f32198e296528b6adb2e86c92937501
Stephen <harkugbeosaz@gmail.com>
2022-04-20 09:31:55 +0100

    Editing the hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,10 +10,10 @@ const Hero = () => {
                     <NavBar />
                 </div>
                 
-                <div className="m-auto max-w-6xl mt-8 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
+                <div className="m-auto overflow-hidden mx-4 mt-8 lg:mt-4 p-2 md:p-12 h-5/6" data-aos="zoom-in">
 
                     <div className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
-                        <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center" data-aos="zoom-in" data-aos-delay="200">
+                        <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">
                             <h1 className="mb-5 md:text-5xl text-3xl font-bold text-blue-900">We build digital solutions to help businesses scale</h1>
                             <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We are a team of highly motivated and skilled developers dedicated to delivering only the best software.</div>
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
--- src/components/Services.js
+++ src/components/Services.js
@@ -11,8 +11,9 @@ const Services = () => {
             <section className="bg-gray-100 py-12" data-aos="zoom-in-down">
                     <div className="my-4 py-4">
                         <h2 className="my-2 text-center text-3xl text-blue-900 uppercase font-bold">services</h2>
+                        
                         <div className='flex justify-center'>
-                            <div className='w-24 border-2 border-blue-900'></div>
+                            <div className='w-24 border-b-4 border-blue-900'></div>
                         </div>
                         <h2 className="mt-4 mx-12 text-center text-xl lg:text-2xl font-semibold text-blue-900">We are deeply committed to the growth and success of our clients.</h2>
                     </div>
