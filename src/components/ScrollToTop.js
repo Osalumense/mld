051e5d10eddba95df05d7baf35a79ab4dd65debf
Akugbe Stephen <harkugbeosaz@gmail.com>
2023-12-27 12:13:00 +0100

    Update ScrollToTop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 const ScrollToTop = (props) => {
     const location = useLocation();
