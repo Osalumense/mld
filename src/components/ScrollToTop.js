e179ec26e7be039828f514be622f85fb738e2406
Akugbe Stephen <harkugbeosaz@gmail.com>
2023-12-27 12:17:15 +0100

    Fix: Removed useLocation from scrollToTop component

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 // import { useLocation } from "react-router-dom";
 
 const ScrollToTop = (props) => {
-    const location = useLocation();
+    // const location = useLocation();
     useEffect(() => {
       window.scrollTo({top: 0, left: 0, behavior: 'smooth' });
     }, []);
