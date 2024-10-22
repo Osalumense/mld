61bc77cceba8c3a7fcc6b112e08333612e439c68
Pranav Achar <73564284+PranavAchar01@users.noreply.github.com>
2024-10-22 15:46:57 +0000

    Pending changes exported from your codespace

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -16,9 +16,15 @@ const NavLinks = () => {
             <HashLink className="px-4 font-extrabold text-black-500 hover:text-blue-900" to="/contact#contact">
                 Contact Us
             </HashLink>
-            <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="https://discord.gg/fGwBVN75">
-                Discord Server
-            </HashLink>
+            <HashLink
+  className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl"
+  smooth
+  to="#"
+  onClick={(e) => { e.preventDefault(); window.open('https://discord.gg/fGwBVN75', '_blank'); }}
+>
+  Discord Server
+</HashLink>
+
         </>
     )
 }
